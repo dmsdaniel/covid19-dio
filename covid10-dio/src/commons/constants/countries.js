28414227f3f1e793d782af7a76ef4c642a497680
Daniel Santos <dmsdaniel@gmail.com>
2020-11-27 21:19:37 -0300

    modified:   .eslintcache

---
+++ covid10-dio/src/commons/constants/countries.js
@@ -0,0 +1,37 @@
+import BrazilFlag from '../../assets/images/brazil.png'
+import GermanyFlag from '../../assets/images/germany.png'
+import ChinaFlag from '../../assets/images/china.png'
+import ItalyFlag from '../../assets/images/italy.png'
+import UsaFlag from '../../assets/images/usa.png'
+
+
+const COUNTRIES = [
+    {
+        label: 'Alemanha',
+        value: 'germany',
+        flag: GermanyFlag
+    },
+    {
+        label: 'Brasil',
+        value: 'brazil',
+        flag: BrazilFlag
+    },
+    {
+        label: 'China',
+        value: 'china',
+        flag: ChinaFlag
+    },
+    {
+        label: 'Estados Unidos',
+        value: 'usa',
+        flag: UsaFlag
+    },
+    {
+        label: 'Itália',
+        value: 'italy',
+        flag: ItalyFlag
+    },
+
+]
+
+export default COUNTRIES
